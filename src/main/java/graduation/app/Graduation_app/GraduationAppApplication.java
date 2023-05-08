e782a6db19004f02f65b42e0e3ad4bd657e2442b
HyunSoo Lee <oceanyum@gmail.com>
2023-05-08 23:54:38 +0900

    master -> main 잘 고쳐졌는지 테스트

--- src/main/java/graduation/app/Graduation_app/GraduationAppApplication.java
+++ src/main/java/graduation/app/Graduation_app/GraduationAppApplication.java
@@ -3,9 +3,15 @@ package graduation.app.Graduation_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class GraduationAppApplication {
-
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GraduationAppApplication.class, args);
 	}
